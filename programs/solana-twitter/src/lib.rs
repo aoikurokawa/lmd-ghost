cefe75f86a23d7b30f2b61702387ed8da9bc2fd1
Aoi <ht.so.ik0102@gmail.com>
2022-03-15 08:21:48 +0700

    change

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -13,6 +13,7 @@ pub mod solana_twitter {
 #[derive(Accounts)]
 pub struct Initialize {}
 
+// 1. Define the structure of the Tweet account.
 #[account]
 pub struct Tweet {
     pub author: Pubkey,
@@ -21,8 +22,21 @@ pub struct Tweet {
     pub content: String,
 }
 
+// 2. Add some useful constants for sizing properties
 const DISCRIMINATOR_LENGTH: usize = 8;
 const PUBLIC_KEY_LENGTH: usize = 32;
 const TIMESTAMP_LENGTH: usize = 8;
-const STRING_LENGTH_PREFIX: usize = 4;
-const MAX_TOPIC_LENGTH: usize = 50 * 4;
+const STRING_LENGTH_PREFIX: usize = 4; // stores the size of the string;
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 characters max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
+
+// 3. Add a constant on the Tweet account that provides its total size
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH
+        + TIMESTAMP_LENGTH
+        + STRING_LENGTH_PREFIX
+        + MAX_TOPIC_LENGTH
+        + STRING_LENGTH_PREFIX
+        + MAX_CONTENT_LENGTH;
+}
