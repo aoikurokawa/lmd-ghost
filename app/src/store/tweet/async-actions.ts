c5c347ba41d0f29692236d9b3147ce71efc443ef
Aoi <ht.so.ik0102@gmail.com>
2022-04-03 13:37:38 +0700

    Declare sendTweet function in async-actions.ts

--- app/src/store/tweet/async-actions.ts
+++ app/src/store/tweet/async-actions.ts
@@ -7,6 +7,7 @@ import {
 import { Program, Provider, web3 } from "@project-serum/anchor";
 
 import idl from "../../idl.json";
+import { Tweet } from "../../models/index";
 
 const network = "http://127.0.0.1:8899";
 
@@ -34,4 +35,26 @@ export const fetchTweets = async () => {
   } catch (error) {
     console.error("ERROR: ", error);
   }
+};
+
+// 1. Define the sendTweet endpoint.
+export const sendTweet = async (topic: string, content: string) => {
+  const provider = getProvider();
+  // @ts-ignore
+  const program = new Program(idl, programID, provider);
+
+  const tweet = web3.Keypair.generate();
+
+  await program.rpc.sendTweet(topic, content, {
+    accounts: {
+      author: provider.wallet.publicKey,
+      tweet: tweet.publicKey,
+      systemProgram: web3.SystemProgram.programId,
+    },
+    signers: [tweet],
+  });
+
+  const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+  return new Tweet(tweet.publicKey, tweetAccount);
 };
