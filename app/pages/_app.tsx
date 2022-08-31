142276a88326a96e22fc8b4bfb9a2994d3753fda
Aoi <ht.so.ik0102@gmail.com>
2022-08-31 20:33:22 +0700

    Change

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,8 @@
+import "../styles/globals.css";
 import React from "react";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -10,31 +10,32 @@ import { clusterApiUrl, ConfirmOptions } from "@solana/web3.js";
 
 import Sidebar from "../src/components/Sidebar";
 import Feed from "../src/components/Feed";
+import { NextPage } from "next";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const endpoint = "http://127.0.0.1:8899";
 
-function App() {
+// const AppWithProvider = () => {
+//   const wallets = [new PhantomWalletAdapter({ network: endpoint })];
+//   return (
+//     <ConnectionProvider endpoint={endpoint}>
+//       <WalletProvider wallets={wallets} autoConnect>
+//         <WalletModalProvider>
+//           <App />
+//         </WalletModalProvider>
+//       </WalletProvider>
+//     </ConnectionProvider>
+//   );
+// };
+
+const Home: NextPage = () => {
   return (
     <div className="flex h-screen max-w-7xl ml-auto mr-auto">
       <Sidebar />
       <Feed />
     </div>
   );
-}
-
-const AppWithProvider = () => {
-  const wallets = [new PhantomWalletAdapter({ network: endpoint })];
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <App />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
 };
 
-export default App;
+export default Home;
