843759b2ac5f693d3f442b7b0422db6713e843c1
Aoi <ht.so.ik0102@gmail.com>
2022-08-31 16:42:26 +0700

    4 tests successfully

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -88,31 +88,33 @@ describe("solana-twitter", () => {
     assert.ok(tweetAccount.timestamp);
   });
 
-  // it("can not provide a topic with more than 50 characters", async () => {
-  //   try {
-  //     // Call the "SendTweet" instruction on behalf of this other user.
-  //     const tweet = anchor.web3.Keypair.generate();
-  //     const topicWith51Chars = "x".repeat(51);
-  //     await program.rpc.sendTweet(topicWith51Chars, "Hummus, am I right?", {
-  //       accounts: {
-  //         tweet: tweet.publicKey,
-  //         author: program.provider.wallet.publicKey,
-  //         systemProgram: anchor.web3.SystemProgram.programId,
-  //       },
-  //       signers: [tweet],
-  //     });
-  //   } catch (error) {
-  //     assert.equal(
-  //       error.msg,
-  //       "The provided topic should be 50 characters long maximum."
-  //     );
-  //     return;
-  //   }
+  it("can not provide a topic with more than 50 characters", async () => {
+    try {
+      const tweet = anchor.web3.Keypair.generate();
+      const topicWith51Chars = "x".repeat(51);
+      await program.methods.sendTweet(topicWith51Chars, "Yay Tofu").accounts(
+        {        
+          tweet: tweet.publicKey,
+          author: program.provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        }
+      )
+      .signers([tweet])
+      .rpc();      
 
-  //   assert.fail(
-  //     "The instruction should have failed with a 51-character topic."
-  //   );
-  // });
+    } catch (error) {
+      console.log("Error: ", error.error);
+      assert.equal(
+        error.error.errorMessage,
+        "The provided topic should be 50 characters long maximum."
+      );
+      return;
+    }
+
+    assert.fail(
+      "The instruction should have failed with a 51-character topic."
+    );
+  });
 
   // it("can not provide a content with more than280 characters", async () => {
   //   try {
