69c063521e9b07cbe01e1a64693dbc11c2446b31
Aoi <ht.so.ik0102@gmail.com>
2022-09-01 10:27:30 +0700

    Update

---
+++ app-v2/components/Feed.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+
+// import Post from "../Post";
+import TweetBox from "./TweetBox";
+// import { IRootState } from "../../store/index";
+// import { fetchTweets } from "../../store/tweet/async-actions";
+
+function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  // const { data } = useSelector((state: IRootState) => state.chain);
+
+  // useEffect(() => {
+  // fetchTweets();
+  // db.collection("posts").onSnapshot((snapshot) => {
+  //   setPosts(snapshot.docs.map((doc) => doc.data()));
+  // });
+  // }, [data]);
+
+  return (
+    <div className="flex-1 min-w-fit overflow-y-scroll">
+      <div className="sticky top-0 bg-white z-50 px-4 py-5">
+        <h2>Home</h2>
+      </div>
+      <TweetBox />
+      {/* {data.walletAddress ? (
+        
+      ) : (
+        <div className="">Connect your wallet to start tweeting...</div>
+      )} */}
+      {/* {posts.map((post) => (
+          <Post
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))} */}
+    </div>
+  );
+}
+
+export default Feed;
--- app-v2/pages/_app.tsx
+++ app-v2/pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  FakeWalletAdapter,
+  PhantomWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import type { FC } from "react";
+import React, { useMemo } from "react";
+
+function App({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      /**
+       * Select the wallets you wish to support, by instantiating wallet adapters here.
+       *
+       * Common adapters can be found in the npm package `@solana/wallet-adapter-wallets`.
+       * That package supports tree shaking and lazy loading -- only the wallets you import
+       * will be compiled into your application, and only the dependencies of wallets that
+       * your users connect to will be loaded.
+       */
+      new PhantomWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default MyApp
+export default App;
